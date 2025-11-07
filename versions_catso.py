e11d1a490a28ccf4d73e8577c4248d3547a51f42
NCat <116185682+NoCat21@users.noreply.github.com>
2025-11-07 10:36:17 +0100

    Fixed a minor bug in CatSo_pop2_bias

--- versions_catso.py
+++ versions_catso.py
@@ -182,6 +182,7 @@ def test_CatSo_var_MaxCut_bench(restart_rate, mean_budget_restart, var_budget_re
     problem = ioh.get_problem(fids[1], problem_class=ioh.ProblemClass.GRAPH)
     best_found_list = []
     print("=" * 20)
+    print(f"{restart_rate}, {mean_budget_restart}, {var_budget_restart}, {budget}")
     for i in range(20):
         CatSo_var_budget_restart(budget=budget, restart_rate=restart_rate, mean_budget_restart=mean_budget_restart, var_budget_restart=var_budget_restart)(problem)
         best_found_list.append(problem.state.current_best.y)
@@ -221,6 +222,9 @@ class CatSo_pop2_bias_rate:
             copy = favorite
             favorite = backup
             backup = copy
+            result_copy = result_fav
+            result_fav = result_backup
+            result_backup = result_copy
 
         # init candidate
         candidate = favorite
@@ -380,4 +384,189 @@ def test_debug(budget):
 
 
 #test_debug(40)
-    
+
+
+
+
+
+
+class CatSo_pop2_bias_rate_plot:
+
+    def __init__(self, budget: int = 50_000, mutation_rate: float = 0.1, restart_rate: float = 0.002,  mean_budget_restart: float = 500, var_budget_restart: float = 5, bias_rate: float = 0.9, **kwargs): # mutation rate, random restart rate, random restart budget rate
+        self.budget = budget
+        self.mutation_rate = mutation_rate
+        self.restart_rate = restart_rate
+        self.mean_budget_restart = mean_budget_restart
+        self.var_budget_restart = var_budget_restart
+        self.bias_rate = bias_rate
+
+    def __call__(self, problem: ioh.problem.IntegerSingleObjective) -> None:
+        
+        trace_favorite = []
+        trace_bias = []
+        trace_restart = []
+
+        self.mutation_rate = 1/problem.meta_data.n_variables
+
+        # Initialize with random sample
+        favorite = np.random.randint(0, 2, size=problem.meta_data.n_variables)
+        backup = np.random.randint(0, 2, size=problem.meta_data.n_variables)
+        result_fav = problem(favorite)
+        result_backup = problem(backup)
+
+        if result_backup >= result_fav: # backup is better than fav, we swap
+            copy = favorite
+            favorite = backup
+            backup = copy
+            result_copy = result_fav
+            result_fav = result_backup
+            result_backup = result_copy
+
+        trace_favorite.append((problem.state.evaluations, ))
+        # init candidate
+        candidate = favorite
+
+
+        # For stats :
+        """
+        count_restart = 0
+        count_useful_restart = 0
+        cost_restart = 0
+        count_backup_picked = 0
+        count_backup_better = 0
+        """
+
+        # For debugging purposes
+        # TO COMMENT
+        # print(f"CatSo called w budget {self.budget}, mut {self.mutation_rate}, restart rate {self.restart_rate}, budget_restart N({self.mean_budget_restart},{self.var_budget_restart})\n")
+
+        # Main loop
+        while problem.state.evaluations < self.budget and not problem.state.optimum_found:
+            
+            # Potential restart
+            if np.random.random() < self.restart_rate:
+                
+                """
+                print("-" * 10)
+                print("restart !")
+                """
+                
+                budget_restart = np.floor(np.random.normal(loc = self.mean_budget_restart, scale = self.var_budget_restart))
+
+
+                # for stats :
+                """
+                if problem.state.evaluations + budget_restart >= self.budget :
+                    cost_restart += self.budget - problem.state.evaluations
+                else :
+                    cost_restart += budget_restart
+                """
+                # print(f"budget restart : {budget_restart}")
+
+                candidate_restart = np.random.randint(0, 2, size=problem.meta_data.n_variables)
+                result_restart = problem(candidate_restart)
+
+                while budget_restart > 0 and problem.state.evaluations < self.budget and not problem.state.optimum_found: # We do have to stay in budget
+                    
+                    budget_restart -= 1
+                    new_candidate_restart = candidate_restart
+
+                    for i in range(len(candidate)):
+                        if np.random.random() < self.mutation_rate:
+                            new_candidate_restart[i] = 1 - new_candidate_restart[i]  # Flip bit (0->1, 1->0)
+                    
+                    new_result_restart = problem(new_candidate_restart)
+
+                    if new_result_restart >= result_restart:
+                        candidate_restart = new_candidate_restart
+                        result_restart = new_result_restart
+                
+                # for stats TO COMMENT :
+                # count_restart +=1
+                
+                """
+                print(f"result restart = {result_restart}")
+                print(f"result favorite = {result_fav}, result backup = {result_backup}")
+                """
+
+                if result_restart >= result_backup :
+                    # for stats TO COMMENT :
+                    # count_useful_restart += 1
+
+                    if result_restart >= result_fav :
+                        # the restart is even better than the favorite. It becomes the fav, and the fav becomes the backup
+                        backup = favorite
+                        result_backup = result_fav
+                        favorite = candidate_restart
+                        result_fav = result_restart
+
+                        # print(f"restart super useful ! result favorite = {result_fav}, result backup = {result_backup}")
+
+                    else :
+                        # the restart is better than the backup. It replaces it
+                        backup = candidate_restart
+                        result_backup = result_restart
+
+                        # print(f"restart useful ! result backup = {result_backup}")
+
+            # Restart finished
+
+            else : # no restart
+                if np.random.random() < self.bias_rate: # we pick the favorite
+                    candidate = favorite
+
+                    for i in range(len(candidate)):
+                        if np.random.random() < self.mutation_rate:
+                            candidate[i] = 1 - candidate[i]  # Flip bit (0->1, 1->0)
+                    
+                    new_result = problem(candidate)
+
+                    if new_result >= result_fav : # new candidate is better than fav, we keep it
+                        favorite = candidate
+                        result_fav = new_result
+                    
+                    """
+                    print("favorite picked")
+                    print(f"result favorite = {result_fav}, result backup = {result_backup}")
+                    """
+
+                else : # we pick the backup
+                    # for stats TO COMMENT :
+                    # count_backup_picked += 1
+
+                    candidate = backup
+
+                    for i in range(len(candidate)):
+                        if np.random.random() < self.mutation_rate:
+                            candidate[i] = 1 - candidate[i]  # Flip bit (0->1, 1->0)
+                    
+                    new_result = problem(candidate)
+
+                    if new_result >= result_backup : # new candidate is better than current backup, we keep it
+                        if new_result >= result_fav : # the backup has become better than the favorite. We swap
+                            # for stats TO COMMENT :
+                            # count_backup_better += 1
+
+                            backup = favorite
+                            favorite = candidate
+                            result_backup = result_fav
+                            result_fav = new_result
+                        else : # we get a better a backup
+                            backup = candidate
+                            result_backup = new_result
+
+                    """
+                    print("backup picked !!!!!")
+                    print(f"result favorite = {result_fav}, result backup = {result_backup}")
+                    """
+                    
+        """
+        print(f"ratio restarts : {count_restart} / {self.budget} = {count_restart / self.budget}")
+        if count_restart > 0 :
+            print(f"ratio useful restarts : {count_useful_restart} / {count_restart} = {count_useful_restart / count_restart}")
+        print(f"cost restart {cost_restart}")
+        if self.budget - cost_restart > 0 :
+            print(f"backup picked : {count_backup_picked} / {self.budget - cost_restart} = {count_backup_picked / (self.budget - cost_restart)}")
+        else : 
+            print("\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!! cost_restart = budget !!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n")
+        """
